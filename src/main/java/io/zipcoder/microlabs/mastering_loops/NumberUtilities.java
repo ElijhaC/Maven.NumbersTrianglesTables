9e530fb730bfa581957e1cce034c9d9e76eab08f
elijhac <ej9314@hotmail.com>
2022-11-02 21:14:09 -0400

    Completed lab

--- src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
@@ -3,34 +3,132 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class NumberUtilities {
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        StringBuilder sb = new StringBuilder();
+        for (int i = start; i < stop; i++) {
+            if (i % 2 == 0) {
+                sb.append(i);
+            }
+        }
+        return sb.toString();
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        StringBuilder sb = new StringBuilder();
+        String result = "";
+        for (int i = start; i < stop; i++) {
+            if (i % 2 != 0) {
+                sb.append(i);
+            }
+        }
+        return sb.toString();
     }
 
 
     public static String getSquareNumbers(int start, int stop, int step) {
-        return null;
+        int result = 1;
+        String square = "";
+        for (int i = start; i < stop; i += step) {
+            result = i * i;
+            square += result;
+        }
+        return square;
     }
 
-    public static String getRange(int start) {
-        return null;
+    public static String getRange(int stop) {
+
+        return getRange(0, stop, 1);
     }
 
     public static String getRange(int start, int stop) {
-        return null;
+
+        return getRange(start, stop, 1);
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = start; i < stop; i += step){
+            sb.append(i);
+        }
+        return sb.toString();
     }
 
 
     public static String getExponentiations(int start, int stop, int step, int exponent) {
-        return null;
+        String expo = "";
+        for (int i = start; i < stop; i += step) {
+            int j = (int) Math.pow(i, exponent);
+            expo += j;
+        }
+        return expo;
     }
 }
+
+//    public static String getEvenNumbers(int start, int stop) {
+//        String result = "";
+//        for (int i = start; i < stop; i++) {
+//            if (i % 2 == 0) {
+//                sb.append(i);
+//            }
+//        }
+//        return sb.toString();
+//    }
+
+
+//    public static String getOddNumbers(int start, int stop) {
+//        String result = "";
+//        for (int i = start; i < stop; i++) {
+//            if (i % 2 != 0) {
+//                sb.append(i);
+//            }
+//        }
+//        return sb.toString();
+//    }
+
+
+//    public static String getSquareNumbers(int start, int stop, int step) {
+//        int result = 1;
+//        String stream = "";
+//        for (int i = start; i < stop; i += step) {
+//            result = i * i;
+//            stream += result;
+//        }
+//        return stream;
+//    }
+//
+//    public static String getRange(int stop) {
+//        String stream = "";
+//        for (int i = 0; i < stop; i++) {
+//            stream += i;
+//        }
+//        return stream;
+//    }
+//
+//    public static String getRange(int start, int stop) {
+//        String stream = "";
+//        for (int i = start; i < stop; i++) {
+//            stream += i;
+//        }
+//        return stream;
+//    }
+//
+//
+//    public static String getRange(int start, int stop, int step) {
+//        String stream = "";
+//        for (int i = start; i < stop; i += step) {
+//            stream += i;
+//        }
+//        return stream;
+//    }
+//
+//
+//    public static String getExponentiations(int start, int stop, int step, int exponent) {
+//        String stream = "";
+//        for (int i = start; i < stop; i += step) {
+//            int j = (int) Math.pow(i, exponent);
+//            stream += j;
+//        }
+//        return stream;
+//    }
